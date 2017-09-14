77a1e44cb7ba82c5869b61e37f5953446af1b5bb
Unknown <alex.pcc.hm@gmail.com>
2017-09-13 20:20:50 -0600

    Updated Math Cs file with comments

--- DGM1600Game/Assets/Math.cs
+++ DGM1600Game/Assets/Math.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Math : MonoBehaviour {
-
+	// stating public variables to be used as input on unity's inspector
 	public float valueOne;
 
 	public float valueTwo;
 
-	private float result;
-
 	public float valueThree;
 
 	public float valueFour;
+
+	// stating private variables to be used with the operators
+	private float result;
 
 	private float resultTwo;
 
@@ -22,9 +23,13 @@ public class Math : MonoBehaviour {
 		
 	}
 	
+	// creating the DoMath function
 	void DoMath () {
 
+		// Stating what result is by using math operators in this example
+		// and using the valueOne and valueTwo variables
 		result = valueOne + valueTwo;
+		// Print the result per desired operation
 		print(valueOne + " + " + valueTwo + " = " + result );
 		result = valueOne - valueTwo;
 		print(valueOne + " - " + valueTwo + " = " + result );
@@ -33,7 +38,10 @@ public class Math : MonoBehaviour {
 		result = valueOne / valueTwo;
 		print(valueOne + " / " + valueTwo + " = " + result );
 
+		// Stating what resultTwo is by using math operators in this example
+		// and using the valueThree and valueFour variables
 		resultTwo = valueThree + valueFour;
+		// print the value of the desired operation
 		print(valueThree + " + " + valueFour + " = " + resultTwo );
 		resultTwo = valueThree - valueFour;
 		print(valueThree + " - " + valueFour + " = " + resultTwo );

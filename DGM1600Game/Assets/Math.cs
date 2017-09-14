441d59fa2bd351820c5e44f9247ee288b3015c21
Unknown <alex.pcc.hm@gmail.com>
2017-09-13 19:57:17 -0600

    Uploading the Math CS file

---
+++ DGM1600Game/Assets/Math.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math : MonoBehaviour {
+
+	public float valueOne;
+
+	public float valueTwo;
+
+	private float result;
+
+	public float valueThree;
+
+	public float valueFour;
+
+	private float resultTwo;
+
+	// Use this for initialization
+	void Start () {
+		DoMath();
+		
+	}
+	
+	void DoMath () {
+
+		result = valueOne + valueTwo;
+		print(valueOne + " + " + valueTwo + " = " + result );
+		result = valueOne - valueTwo;
+		print(valueOne + " - " + valueTwo + " = " + result );
+		result = valueOne * valueTwo;
+		print(valueOne + " * " + valueTwo + " = " + result );
+		result = valueOne / valueTwo;
+		print(valueOne + " / " + valueTwo + " = " + result );
+
+		resultTwo = valueThree + valueFour;
+		print(valueThree + " + " + valueFour + " = " + resultTwo );
+		resultTwo = valueThree - valueFour;
+		print(valueThree + " - " + valueFour + " = " + resultTwo );
+		resultTwo = valueThree * valueFour;
+		print(valueThree + " * " + valueFour + " = " + resultTwo );
+		resultTwo = valueThree / valueFour;
+		print(valueThree + " / " + valueFour + " = " + resultTwo );
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

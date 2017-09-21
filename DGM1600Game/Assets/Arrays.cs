e32a2ea262d0a1652e7956db5b29c76049003e53
Unknown <alex.pcc.hm@gmail.com>
2017-09-20 20:14:54 -0600

    Array

---
+++ DGM1600Game/Assets/Arrays.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public string[] heroes= new string[6];
+
+	// public RedShop[]inventory= new RedShop[6];
+	// inventory [0]= health potion;
+	// inventory [1]= mana potion;
+	// inventory [2]= reload ammo;
+	// inventory [3]= repair armor;
+	// inventory [4]= hire warrior; 
+	// inventory [5]= rent vehicle;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	heroes [0]="Batman";
+	heroes [1]="Superman";
+	heroes [2]="Flash";
+	heroes [3]="Hulk";
+	heroes [4]="Wonder Woman";
+	heroes [5]="Wolverine";
+
+	print(heroes[0]);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
